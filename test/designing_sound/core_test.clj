06d5d9b56f1d6029c9523253b6a31b750c3f86a9
Dane Filipczak <dane@imaginationrock.com>
2019-11-17 10:38:16 -0800

    initial commit

---
+++ test/designing_sound/core_test.clj
@@ -0,0 +1,7 @@
+(ns designing-sound.core-test
+  (:require [clojure.test :refer :all]
+            [designing-sound.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
